39d99a2c67e86cd5ebd19de91db4249430b2836c
somil1992 <33191690+somil1992@users.noreply.github.com>
2021-12-14 17:31:17 +0530

    Update fmodel2.py

--- fmodel2.py
+++ fmodel2.py
@@ -4,10 +4,33 @@ import pickle
 import numpy as np
 import json
 import pandas as pd
+import glob
+import os.path
+
 # creating the flask app
 app = Flask(__name__)
 # creating an API object
 api = Api(app)
+
+# All files ending with .pkl in source_model
+model_path = 'source_model\\*pkl'
+model_files = glob.glob(model_path)
+max_model_file = max(model_files, key=os.path.getctime)
+
+model_dst= 'model\\model.pkl'
+shutil.copy(max_model_file,model_dst)
+
+
+# All files ending with .csv in source_config
+config_path = 'source_config\\*csv'
+config_files = glob.glob(config_path)
+max_config_file = max(config_files, key=os.path.getctime)
+
+config_dst= 'config\\config.csv'
+shutil.copy(max_config_file,config_dst)
+
+
+
 
 #Loading Models
 model = pickle.load(open('model/model.pkl', 'rb'))
