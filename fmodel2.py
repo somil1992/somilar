38f8f8be13f26c22db914db5f66b964aa1f4a966
somil1992 <33191690+somil1992@users.noreply.github.com>
2021-12-14 18:08:36 +0530

    Update fmodel2.py

--- fmodel2.py
+++ fmodel2.py
@@ -15,54 +15,55 @@ api = Api(app)
 # All files ending with .pkl in source_model
 model_path = 'source_model\\*pkl'
 model_files = glob.glob(model_path)
-max_model_file = max(model_files, key=os.path.getctime)
+print(model_files)
+# max_model_file = max(model_files, key=os.path.getctime)
 
-model_dst= 'model\\model.pkl'
-shutil.copy(max_model_file,model_dst)
-
-
-# All files ending with .csv in source_config
-config_path = 'source_config\\*csv'
-config_files = glob.glob(config_path)
-max_config_file = max(config_files, key=os.path.getctime)
-
-config_dst= 'config\\config.csv'
-shutil.copy(max_config_file,config_dst)
+# model_dst= 'model\\model.pkl'
+# shutil.copy(max_model_file,model_dst)
 
 
+# # All files ending with .csv in source_config
+# config_path = 'source_config\\*csv'
+# config_files = glob.glob(config_path)
+# max_config_file = max(config_files, key=os.path.getctime)
+
+# config_dst= 'config\\config.csv'
+# shutil.copy(max_config_file,config_dst)
 
 
-#Loading Models
-model = pickle.load(open('model/model.pkl', 'rb'))
-
-#loading configrations
-config=pd.read_csv('config/config.csv')
 
 
-# making a class for a particular resource
-# the get, post methods correspond to get and post requests
-# they are automatically mapped by flask_restful.
+# #Loading Models
+# model = pickle.load(open('model/model.pkl', 'rb'))
+
+# #loading configrations
+# config=pd.read_csv('config/config.csv')
 
 
-class Predict(Resource):
-    def post(self):
-        #Definition
-        dict={}
+# # making a class for a particular resource
+# # the get, post methods correspond to get and post requests
+# # they are automatically mapped by flask_restful.
+
+
+# class Predict(Resource):
+#     def post(self):
+#         #Definition
+#         dict={}
     
-        for i in range(len(config)):
-            #param = config.iloc[i,0]
-            #dict["var{0}".format(i+1)] = request.args.get(param)
-            dict["var{0}".format(i+1)] = request.args.get(config.iloc[i,0])
+#         for i in range(len(config)):
+#             #param = config.iloc[i,0]
+#             #dict["var{0}".format(i+1)] = request.args.get(param)
+#             dict["var{0}".format(i+1)] = request.args.get(config.iloc[i,0])
             
-        values = dict.values()
-        values_list = list(values)
+#         values = dict.values()
+#         values_list = list(values)
 
-        prediction = model.predict([values_list])
-        return jsonify(prediction.tolist())
+#         prediction = model.predict([values_list])
+#         return jsonify(prediction.tolist())
 
 
 
-api.add_resource(Predict, '/predict')
+# api.add_resource(Predict, '/predict')
 
 # driver function
 if __name__ == '__main__':
