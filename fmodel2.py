36d9fbbde312cbab4b63578d28707cea6bf8c1ec
somil1992 <33191690+somil1992@users.noreply.github.com>
2021-12-16 12:07:44 +0530

    Update fmodel2.py

--- fmodel2.py
+++ fmodel2.py
@@ -88,4 +88,4 @@ api.add_resource(Predict, '/predict')
 
 # driver function
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000,debug = True)
+    app.run(host="0.0.0.0",port=5000,debug = False)
