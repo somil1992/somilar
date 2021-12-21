3297f2afa89577df9dc5e1e786fa57dceec9e35d
somil1992 <33191690+somil1992@users.noreply.github.com>
2021-12-22 00:46:37 +0530

    Update fmodel2.py

--- fmodel2.py
+++ fmodel2.py
@@ -12,27 +12,6 @@ app = Flask(__name__)
 # creating an API object
 api = Api(app)
 
-# All files ending with .pkl in source_model
-# model_path = 'source_model\\*pkl'
-# model_files = glob.glob(model_path)
-# print(model_files)
-# max_model_file = max(model_files, key=os.path.getctime)
-
-# model_dst= 'model\\model.pkl'
-# shutil.copy(max_model_file,model_dst)
-
-
-# # All files ending with .csv in source_config
-# config_path = 'source_config\\*csv'
-# config_files = glob.glob(config_path)
-# max_config_file = max(config_files, key=os.path.getctime)
-
-# config_dst= 'config\\config.csv'
-# shutil.copy(max_config_file,config_dst)
-
-
-
-
 
 
 # # making a class for a particular resource
@@ -51,14 +30,16 @@ class Predict(Resource):
         Post reading the inputs - model file is utilized to forecast the output.
         '''
 
-        model_name = request.args.get("model")
-        config_name = request.args.get("config")
+        model_name = request.form.get("model")
+        config_name = request.form.get("config")
+        input_format = request.form.get("input_format")
         
         model_filename = model_name + ".pkl"
         config_filename = config_name + ".csv"
-
+   
         #Loading Models
-        model = pickle.load(open('source_model/' + model_filename , 'rb'))
+        #model = pickle.load(open('source_model/' + model_filename , 'rb'))
+        model = joblib.load('source_model/' + model_filename )
 
         #loading configrations
         config = pd.read_csv('source_config/' + config_filename)
@@ -66,20 +47,27 @@ class Predict(Resource):
         print(config)
         print(model)
 
-        #Definition
-        dict={}
-    
-        for i in range(len(config)):
-            #param = config.iloc[i,0]
-            #dict["var{0}".format(i+1)] = request.args.get(param)
-            dict["var{0}".format(i+1)] = float(request.args.get(config.iloc[i,0]))
-            
-        values = dict.values()
-        values_list = list(values)
-        print(values_list)
-        prediction = model.predict([values_list])
+        if input_format == 'value' :
+            #Definition
+            dict={}    
+            for i in range(len(config)):
+                dict["var{0}".format(i+1)] = float(request.form.get(config.iloc[i,0]))
+                
+            values = dict.values()
+            values_list = list(values)
+            print(values_list)
+            prediction = model.predict([values_list])
+
+        elif input_format == 'file' :
+            data = request.files["file"]
+            data = pd.read_csv(data)
+
+            prediction = model.predict(data)
+
+
+
         return jsonify(prediction.tolist())
-        # return 0
+
 
 
 
@@ -88,4 +76,4 @@ api.add_resource(Predict, '/predict')
 
 # driver function
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000,debug = False)
+    app.run(host="0.0.0.0",port=5000,debug=False)
